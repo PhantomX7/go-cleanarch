9e89b3dcc2968e7cdc57cd2b752305ea743c568c
Kenichi <kenichixie@gmail.com>
2020-10-06 10:37:15 +0700

    update r table migration

--- migrations/20201006033112-create-r-table.js
+++ migrations/20201006033112-create-r-table.js
@@ -2,13 +2,35 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    id: {
+      allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+    },
+    name: {
+      allowNull: true,
+          unique: true,
+          type: Sequelize.STRING
+    },
+    author_id: {
+      allowNull: false,
+          references: {
+        model: {
+          tableName: 'authors',
+        },
+        key: 'id'
+      },
+      type: Sequelize.INTEGER
+    },
+    created_at: {
+      allowNull: false,
+          type: Sequelize.DATE
+    },
+    updated_at: {
+      allowNull: false,
+          type: Sequelize.DATE
+    }
   },
 
   down: (queryInterface, Sequelize) => {
